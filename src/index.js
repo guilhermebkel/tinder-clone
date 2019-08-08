2465d97a05ab4d4f8aa08f715ce6e77f2014e248
Guilherme <guilhermebromonschenkel@gmail.com>
2019-08-07 21:59:02 -0300

    Added react router

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import './global.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Route exact path='/' component={App} />
+    </BrowserRouter>
+, document.getElementById('root'));
 
 serviceWorker.unregister();
