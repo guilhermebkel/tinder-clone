20d8665f8e41bf63439b86696514f5b5d21dd103
Guilherme <guilhermebromonschenkel@gmail.com>
2019-08-08 00:40:04 -0300

    Changed gh-pages version and published online website

---
+++ precache-manifest.74d077316a41897e74a60adf64556ceb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "324661674d79156b676e63bb5dd3e167",
+    "url": "/Fake-Tinder/index.html"
+  },
+  {
+    "revision": "3f6022cfc078857f109e",
+    "url": "/Fake-Tinder/static/css/main.1d44caa7.chunk.css"
+  },
+  {
+    "revision": "c3d504ffad00e2f9ce58",
+    "url": "/Fake-Tinder/static/js/2.96e18159.chunk.js"
+  },
+  {
+    "revision": "3f6022cfc078857f109e",
+    "url": "/Fake-Tinder/static/js/main.c68e0858.chunk.js"
+  },
+  {
+    "revision": "902b97590a53341d4157",
+    "url": "/Fake-Tinder/static/js/runtime~main.3e4ef5a4.js"
+  },
+  {
+    "revision": "d4ec34e81346484c83328c449a35141b",
+    "url": "/Fake-Tinder/static/media/dislike.d4ec34e8.svg"
+  },
+  {
+    "revision": "542059a425f4220819edb0e56c735bb7",
+    "url": "/Fake-Tinder/static/media/like.542059a4.svg"
+  },
+  {
+    "revision": "6a01ceef657ab22cc670a0a1ae108176",
+    "url": "/Fake-Tinder/static/media/logo.6a01ceef.svg"
+  }
+]);
